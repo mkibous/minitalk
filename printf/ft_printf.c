fbe0d2e13595e5c561ba22e94a63ce650a16091b
Mehdi Kibous <mkibous@e3r6p2.1337.ma>
2024-02-16 22:13:10 +0100

    minitalk

---
+++ printf/ft_printf.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkibous <mkibous@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 15:13:50 by mkibous           #+#    #+#             */
+/*   Updated: 2024/01/24 18:08:55 by mkibous          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *str, ...)
+{
+	int		i;
+	int		l;
+	va_list	args;
+
+	va_start(args, str);
+	i = 0;
+	l = 0;
+	if (write(1, "", 0) == -1)
+		return (-1);
+	while (str[i])
+	{
+		if (str[i] == '%' && str[i + 1])
+		{
+			i++;
+			ft_arg(args, str[i], &l);
+		}
+		else if (str[i] != '%')
+			ft_putchar(str[i], &l);
+		i++;
+	}
+	va_end(args);
+	return (l);
+}
